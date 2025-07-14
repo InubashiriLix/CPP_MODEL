c89e5c5a5af670b4607951c52c4e1ee11d92e6a1
InubashiriLix <lixinrong4002@gmail.com>
2025-07-14 19:26:35 +0800

    for windows msys mingw + cmake

---
+++ src/main.cc
@@ -0,0 +1,9 @@
+#include "add.hpp"
+#include <iostream>
+
+int main() {
+  std::cout << add(2, 3) << std::endl;
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << "shit" << std::endl;
+  return 0;
+}
