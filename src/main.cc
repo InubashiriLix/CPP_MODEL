8c7ebe9be3d2b99454d3d6507c8ab81ec26e0dd2
InubashiriLix <lixinrong4002@gmail.com>
2025-07-20 03:30:16 +0800

    update ignore + CmakeList robust

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,9 @@
-#include "add.hpp"
 #include <iostream>
 
-int main() {
+#include "add.hpp"
+
+int main()
+{
   std::cout << add(2, 3) << std::endl;
   std::cout << "Hello, World!" << std::endl;
   std::cout << "shit" << std::endl;
